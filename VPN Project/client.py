9f9aee97527b603cc6f494fcf0d4e3fdbbf0cbb5
theHorseman <134142510+theHorseman123@users.noreply.github.com>
2023-12-11 15:56:02 +0200

    Update client.py

--- VPN Project/client.py
+++ VPN Project/client.py
@@ -1,17 +1,29 @@
 import socket as sock
+from _thread import start_new_thread
+
+def handler(s:sock):
+    try:
+        while 1:
+            data = s.recv(1024)
+            if data:
+                print(f"\nProxy: <{data.decode('utf-8')}>")
+            else:
+                s.close()
+                break
+    except:
+        s.close()
+
 
 # Creating client socket
 client = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 # Connection to proxy
 client.connect(("localhost", 8080))
 
-while 1:
-    msg = input("insert message: ")
-    client.send(msg.encode("utf-8"))
-    data = client.recv(1024)
-    print(data)
-    if data:
-        print(f"Proxy: <{data.decode('utf-8')}>")
-    else:
-        client.close()
-        break
+print("Connected to proxy on: ", ("127.0.0.1", 8080))
+start_new_thread(handler, (client, ))
+try:
+    while 1:
+        msg = input("insert message: ")
+        client.send(msg.encode("utf-8"))
+except:
+    pass
