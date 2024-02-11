50f420a88537f803a2f61c6d09c3faa6bc950889
theHorseman <134142510+theHorseman123@users.noreply.github.com>
2024-02-11 06:21:26 +0200

    Update proxy.py

--- VPN Project/proxy.py
+++ VPN Project/proxy.py
@@ -1,74 +1,68 @@
-from socket import *
+import socket as sock
+import scapy.all
 from select import select
+from _thread import start_new_thread
+
 
 class Proxy:
 
-    def __init__(self, host:str="localhost", port:int = 8080, server_host:str ="localhost", server_port:int=1234) -> None:
-        
-        self.proxy = socket(AF_INET, SOCK_STREAM)
-        self.proxy.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    def __init__(self, host="127.0.0.1", port=8080) -> None:
         self.host = host
         self.port = port
 
-        self.server_host = server_host
-        self.server_port = server_port
-
-    def start(self) -> None:
-
-        # connecting to server
-        server = socket(AF_INET, SOCK_STREAM)
-        server.connect((self.server_host, self.server_port))
-        
-        # bind socket to address 
-        self.proxy.bind((self.host, self.port))
-        self.proxy.listen(5)
-
-        rlist = [server]
-
-        print("Starting proxy on:", (self.host, self.port))
-
+    def client_handler(self, client_sock, addr):
         while 1:
-            # get readable and writeable socket 
-            read_socks, write_socks, _ = select([self.proxy]+rlist, rlist, [])
-            for read_sock in read_socks:
-                try:
-                    # if a new client tries to connect
-                    if read_sock is self.proxy:
-                        sock, addr = read_sock.accept()
-                        print("\nNew connection from: ", (addr))
-                        rlist.append(sock)
-                        continue
-                    data = read_sock.recv(1024)
-                    if data:
-                        if read_sock == server:
-                            print(f"Server: <{data.decode('utf-8')}>")
-                            # for now the proxy will broadcast the server's messages because there is only one server
-                            for client in rlist:
-                                if not client == server:
-                                   client.send(data)
-                        else:
-                            # forward message to server
-                            print(f"Client: <{data.decode('utf-8')}>")
-                            server.send(data)
-                    else:
-                        print(1)
-                        print(f"Closing connection with client")
-                        rlist.remove(read_sock)
-                        read_sock.close()
-                    
-                except Exception as error:
-                    print(error)
-                    print(f"Closing connection with client")
-                    rlist.remove(read_sock)
-                    read_sock.close()
-    
+            request = client_sock.recv(1024)
+            print(request)
+
+    def mainloop(self):
+        proxy_sock = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        proxy_sock.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1)
+
+        try:
+            proxy_sock.bind((self.host, self.port))
+            proxy_sock.listen(5)
+            while 1:
+                print("Starting proxy server")
+                client_sock, addr = proxy_sock.accept()
+                print("New connection from:",(addr))
+                start_new_thread(self.client_handler, (client_sock, addr))
+
+        except Exception as error:
+            raise error
+
+    def notify_server(self, server_ip = "localhost", server_port = 1234):
+        
+        sock_proxy = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        sock_proxy.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1)
+
+        try:
+            print()
+            sock_proxy.bind((server_ip, 0))
+            sock_proxy.connect((server_ip, server_port))
+            print("connection established with server at:", (server_ip, server_port))
+            
+            sock_proxy.send(f"proxy/{self.host}/{self.port}".encode('utf-8'))
+            data = sock_proxy.recv(1024)
+            if not data == b"illegal" and not data == b"":
+                print("notified successfully")
+                sock_proxy.close()
+
+            else:
+                print("The server is offline")
+                sock_proxy.close()
+
+        except Exception as error:
+            sock_proxy.close()
+            raise error
+
 def main():
     # Create proxy object
     proxy = Proxy()
 
-    
     # Start proxy
-    proxy.start()
-
+    proxy.notify_server()
+    proxy.mainloop()
+    
 if __name__ == '__main__':
     main()
