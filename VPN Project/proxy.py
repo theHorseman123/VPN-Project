e15bec30755a9296eeffe4c022570c08c84f282f
theHorseman <134142510+theHorseman123@users.noreply.github.com>
2024-02-11 09:15:51 +0200

    Update proxy.py

--- VPN Project/proxy.py
+++ VPN Project/proxy.py
@@ -1,5 +1,6 @@
 import socket as sock
 import scapy.all
+import re
 from select import select
 from _thread import start_new_thread
 
@@ -10,10 +11,112 @@ class Proxy:
         self.host = host
         self.port = port
 
+
+    def https_request(self, client_sock, addr, request):
+        
+        web_host, web_port = request.split(b" ")[1].split(b":")
+        request_sock = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        
+        try:
+            request_sock.connect((web_host, int(web_port)))
+            print("")
+        except Exception as error:
+            print("Couldn't Connect web server at:", (web_host, web_port))
+            client_sock.send(str(error).encode("utf-8"))
+            client_sock.close()
+        
+        reply = "HTTP/1.0 200 Connection established\r\nProxy-agent: TheHorseMan_Tunnel\r\n\r\n"
+        client_sock.send(reply.encode())
+
+        request_sock.setblocking(False)
+        client_sock.setblocking(False)
+
+        # transfer messages back and forth
+        while 1:
+            try:
+                data = client_sock.recv(4098)
+                request.sendall(data)
+            except BlockingIOError as error:
+                pass
+            except Exception as error:
+                break
+
+            try:
+                data = request.recv(4098)
+                client_sock.sendall(data)
+            except BlockingIOError as error:
+                pass
+            except Exception as error:
+                break
+
+        request_sock.close()
+        client_sock.close()
+    """
+    def http_request(self, client_sock, addr, request):
+        request = request.decode('utf-8')
+        request_lines = request.split("\r\n")
+        host_line = [i for i in request_lines if "Host:" in i][0]
+        web_host = host_line.split(":")
+        
+        if len(web_host) < 1:
+            print("something wrong with request")
+            client_sock.close()
+            return
+        
+        web_host = web_host[1].strip()
+
+        # search for a port match using regex
+        
+        web_port = 80
+        
+        request_sock = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        
+        # getting requested data from client and sending it to the server
+        reply = request[:request.find(b" ") + 1] + request[request.find(b"/",request.find(b"//") + 2):]
+        try:
+            request_sock.sendall(reply)
+        except:
+            client_sock.close()
+            request_sock.close()
+            return
+
+        data = b''
+        data_fragment = b'1'
+
+        # getting data from webserver
+        while data_fragment:
+            try:
+                data_fragment = request_sock.recv(4096)
+            except:
+                break
+
+            data = data + data_fragment
+        else:
+            try:
+                client_sock.sendall(data)
+            except:
+                client_sock.close()
+        
+        request_sock.close()
+        client_sock.close()
+        """
+
+
     def client_handler(self, client_sock, addr):
         while 1:
-            request = client_sock.recv(1024)
-            print(request)
+            try:
+                request = client_sock.recv(4098)
+                if request:
+                    http_method = request.split(b" ")[0]
+                    if http_method == "CONNECT":
+                        self.https_request(client_sock, addr, request)
+                    else:
+                        self.http_request(client_sock, addr, request)
+                else:
+                    client_sock.close()
+                    break 
+            except Exception as error:
+                raise error
 
     def mainloop(self):
         proxy_sock = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
@@ -58,10 +161,10 @@ class Proxy:
 
 def main():
     # Create proxy object
-    proxy = Proxy()
+    proxy = Proxy(host="10.100.102.93")
 
     # Start proxy
-    proxy.notify_server()
+    proxy.notify_server(server_ip="10.100.102.93")
     proxy.mainloop()
     
 if __name__ == '__main__':
