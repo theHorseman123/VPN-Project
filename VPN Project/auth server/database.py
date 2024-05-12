adaf351b0d0e60c351b7b589cfc4c5a56a30df53
theHorseman <134142510+theHorseman123@users.noreply.github.com>
2024-05-13 02:03:41 +0300

    Update database.py

--- VPN Project/auth server/database.py
+++ VPN Project/auth server/database.py
@@ -4,28 +4,52 @@ class Clients:
 
     def __init__(self) -> None:
         # Connect to users database
-        self.__users = lite.connect('users.db')
+        self.__users = lite.connect('./auth server/users.db')
 
     def __create_table(self):
+        
+        try:
+            cursor = self.__users.cursor()
+            cursor.execute("""CREATE TABLE IF NOT EXISTS users
+                    (
+                    user text primary key,
+                    password text,
+                    active int
+                    )""")
+            
+            cursor.execute("""UPDATE users SET active=0 WHERE active=1""")
 
-        c = self.__users.cursor()
-        c.execute("""CREATE TABLE IF NOT EXISTS users
-                  (
-                  email text primary key,
-                  password text,
-                  addr text
-                  )""")
-        self.__users.commit()
-        c.close()
+            self.__users.commit()
+            cursor.close()
+        except lite.Error:
+            print("datbase error: %s" % str(lite.Error))
 
     def add_user(self, data):
-        pass
+        try:
+            cursor = self.__users.cursor()
+            cursor.execute("""INSERT INTO users user, password, active VALUES(?, ? ,?, 1)""", (data[0], data[1]))
+            cursor.commit()
+        except lite.Error:
+            print("datbase error: %s" % str(lite.Error))
+            return "database error"
+        return "pass"
 
     def get_users(self, data):
-        pass
+        try:
+            cursor = self.__users.cursor()
+            cursor.execute("SELECT user, addr FROM users WHERE 1=1")
+            rows = cursor.fetchall()
+        except lite.Error:
+            print("datbase error: %s" % str(lite.Error))
+            return "database error"
+        return rows
 
     def login(self, data):
-        pass
+        try:
+            cursor = self.__users.cursor()
+            cursor.execute("""SELECT user FROM users WHERE user=? AND password=?""")
+        except:
+            pass
 
 
 
@@ -33,37 +57,85 @@ class Proxies:
     
     def __init__(self) -> None:
         # Connect to proxies database
-        self.__proxies = lite.connect('proxis.db')
+        self.__proxies = lite.connect('./auth server/proxies.db')
 
     def __create_table(self):
-        c = self.__proxies.cursor()
+        cursor = self.__proxies.cursor()
 
-        c.execute("""CREATE TABLE IF NOT EXISTS proxies
+        cursor.execute("""CREATE TABLE IF NOT EXISTS proxies
                   (
-                  email text primary key,
+                  user text primary key
                   password text,
                   addr text,
                   locked int,
+                  key text,
                   active int
                   )""")
         
-        c.execute("""UPDATE proxies SET active=0 WHERE active==1 """)
+        cursor.execute("""UPDATE proxies SET active=0 WHERE active=1 """)
         
         self.__proxies.commit()
-        c.close()
+        cursor.close()
         
 
     def get_active_proxies(self):
-        c = self.__proxies.cursor()
+        cursor = self.__proxies.cursor()
 
-        c.execute("""SELECT host, port FROM Proxies WHERE active=1""")
+        cursor.execute("""SELECT addr, locked FROM proxies WHERE active=1""")
+
+        proxies = cursor.fetchall()
+        cursor.close()
+
+        return proxies
 
     def update_active_proxy(self, data):
-        pass
+        cursor = self.__proxies.cursor()
+        cursor.execute("""SELECT locked FROM proxies WHERE user=? AND password=?""")
+        if cursor.fetchall() == None:
+            cursor.close()
+            return "an error occured"
+        
+        if len(data) == 3: # update activity
+            cursor.execute("""UPDATE proxies SET addr=?, active=1 WHERE user=?""", (data[2], data[0]))
+            cursor.commit()
+            cursor.close()
 
+            return "pass"
+        if len(data) == 4: # update proxie lock code
+            cursor.execute("""UPDATE proxies SET code=? locked=1 WHERE user=?""", (data[2], data[0]))
+            cursor.commit()
+            cursor.close()
+            
+            return "pass"
+        
     def add_proxy(self, data):
-        pass
+        try:
+            cursor = self.__proxies.cursor()
+            if len(data) == 3:
+                cursor.execute("""INSERT INTO proxies user, password, addr VALUES(?, ?, ?)""", (data[0], data[1], data[2]))
+            elif len(data) == 4:
+                cursor.execute("""INSERT INTO proxies user, password, addr, locked, code VALUES(?, ?, ?, 1, ?)""", (data[0], data[1], data[2], data[3]))
+            
+            cursor.commit()
+            
+        except lite.Error as error:
+            print(f"Error in database: {str(error)}")
+            cursor.close()
+            return "an error occured"
+        
+        cursor.close()
+        return "pass"
 
-    def remove_proxy(self, data):
-        pass
+    def remove_proxy(self, user):
+        try:
+            # TODO: make it much more secured, maybe with encryption
+            cursor = self.__proxies.cursor()
+            cursor.execute("""DELETE FROM proxies WHERE user=?""", (user, ))
+        except lite.error as error:
+            print(f"Error in database: {str(error)}")
+            cursor.close()
+            return "an error occured"
+        
+        cursor.close()
+        return "pass"
 
